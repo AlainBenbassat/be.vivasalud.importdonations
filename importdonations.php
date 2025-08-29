cbcbccd96a9f55ad4332c406ad740c95dcc32965
Alain Benbassat <alain@businessandcode.eu>
2025-08-29 14:39:23 +0200

    updated components

--- importdonations.php
+++ importdonations.php
@@ -22,59 +22,12 @@ function importdonations_civicrm_install() {
 }
 
 /**
- * Implements hook_civicrm_postInstall().
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_postInstall
- */
-function importdonations_civicrm_postInstall() {
-  _importdonations_civix_civicrm_postInstall();
-}
-
-/**
- * Implements hook_civicrm_uninstall().
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_uninstall
- */
-function importdonations_civicrm_uninstall() {
-  _importdonations_civix_civicrm_uninstall();
-}
-
-/**
  * Implements hook_civicrm_enable().
  *
  * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_enable
  */
 function importdonations_civicrm_enable() {
   _importdonations_civix_civicrm_enable();
-}
-
-/**
- * Implements hook_civicrm_disable().
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_disable
- */
-function importdonations_civicrm_disable() {
-  _importdonations_civix_civicrm_disable();
-}
-
-/**
- * Implements hook_civicrm_upgrade().
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_upgrade
- */
-function importdonations_civicrm_upgrade($op, CRM_Queue_Queue $queue = NULL) {
-  return _importdonations_civix_civicrm_upgrade($op, $queue);
-}
-
-/**
- * Implements hook_civicrm_entityTypes().
- *
- * Declare entity types provided by this module.
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_entityTypes
- */
-function importdonations_civicrm_entityTypes(&$entityTypes) {
-  _importdonations_civix_civicrm_entityTypes($entityTypes);
 }
 
 // --- Functions below this ship commented out. Uncomment as required. ---
